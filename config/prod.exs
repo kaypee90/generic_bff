d501df3b833e1ccbef7d8e7d2b64e4135b720037
unknown <kaypee90@yahoo.com>
2020-01-07 18:26:38 +0000

    Add heroku configuration

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,11 @@ use Mix.Config
 # which you typically run after static files are built.
 config :my_bff, MyBff.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  load_from_system_env: true,
+  url: [scheme:"https", host: "genericbff.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
